1ab71f630c83955fcb0fa4cf910414fc70d8e361
Noman-Bhatti <nomanbhatti01@outlook.com>
2025-11-23 18:37:53 +0000

    #5 Including CRUDL state

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,28 +1,30 @@
+import { use, useState } from "react";
 import { StyleSheet } from "react-native"; // Added View import
 import Screen from "../layout/Screen";
 import ModuleList from "../entity/modules/ModuleList.js";
+import RenderCount from "../UI/RenderCount.js";
 
 import initaialModules from "../../data/modules.js";
 
 const ModuleListScreen = () => {
   // Initalisations --------------------------------------------
-  const modules = initaialModules;
 
   // States ---------------------------------------------------
+  const [modules, setModules] = useState(initaialModules);
 
   // Handlers -------------------------------------------------
-  const handleSelect = (module) =>
-    alert(`Module ${module.ModuleCode} selected`);
+  const handleDelete = (module) =>
+    setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
+
   // View -----------------------------------------------------
   return (
     <Screen>
-      <ModuleList modules={modules} onSelect={handleSelect} />
+      <RenderCount />
+      <ModuleList modules={modules} onSelect={handleDelete} />
     </Screen>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {},
-});
+const styles = StyleSheet.create({});
 
 export default ModuleListScreen;
